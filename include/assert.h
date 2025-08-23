5179501ec2568a70f818cf05d9f5f8f7a330c987
someone2639 <someone2639@gmail.com>
2025-08-23 16:30:00 -0400

    fix assertf with only format str

--- include/assert.h
+++ include/assert.h
@@ -8,7 +8,7 @@
 extern void __assert(const char*, const char*, int);
 extern void __assertf(const char* exp, const char* filename, int line, const char* fmt, ...);
 #define assert(EX)            ((EX) ? ((void)0) : __assert(#EX, __FILE__, __LINE__))
-#define assertf(EX, fmt, ...) ((EX) ? ((void)0) : __assertf(#EX, __FILE__, __LINE__, fmt, __VA_ARGS__))
+#define assertf(EX, fmt, ...) ((EX) ? ((void)0) : __assertf(#EX, __FILE__, __LINE__, fmt , ## __VA_ARGS__))
 #endif /* NDEBUG */
 
 #endif /* !__ASSERT_H__ */
